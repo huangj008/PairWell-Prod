3a0485a9f97c0eeba7f0363742363e5646c9f515
Jenny Huang <48764041+huangj008@users.noreply.github.com>
2026-04-08 15:15:16 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,12 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+
+  build: {
+    rollupOptions: {
+      external: [
+        /^figma:asset\//,
+      ],
+    },
+  },
 })
